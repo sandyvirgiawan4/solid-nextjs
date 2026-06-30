d1c85a40ff4766d0c96ea845cad81a0fce243667
sandyvirgiawan4 <sandyvirgiawan4@gmail.com>
2026-06-30 08:32:12 +0700

    Update index.tsx

--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -7,33 +7,30 @@ import SectionHeader from "../Common/SectionHeader";
 const Feature = () => {
   return (
     <>
-      {/* <!-- ===== Features Start ===== --> */}
+      {/* */}
       <section id="features" className="py-20 lg:py-25 xl:py-30">
         <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0">
-          {/* <!-- Section Title Start --> */}
+          {/* */}
           <SectionHeader
             headerInfo={{
-              title: "SOLID FEATURES",
-              subtitle: "Core Features of Solid",
-              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-            convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam
-            ante in maximus.`,
+              title: "KEUNGGULAN KAMI",
+              subtitle: "Mengapa Memilih PT JTS?",
+              description: `Kami berkomitmen memberikan layanan internet terbaik dengan infrastruktur modern, interkoneksi handal, dan dukungan teknis yang siap menjaga kelancaran bisnis dan aktivitas digital Anda.`,
             }}
           />
-          {/* <!-- Section Title End --> */}
+          {/* */}
 
           <div className="mt-12.5 grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:mt-15 lg:grid-cols-3 xl:mt-20 xl:gap-12.5">
-            {/* <!-- Features item Start --> */}
-
+            {/* */}
             {featuresData.map((feature, key) => (
               <SingleFeature feature={feature} key={key} />
             ))}
-            {/* <!-- Features item End --> */}
+            {/* */}
           </div>
         </div>
       </section>
 
-      {/* <!-- ===== Features End ===== --> */}
+      {/* */}
     </>
   );
 };
