c9bc6772448549a6b50d6cfb14ab8a0f3bff7018
sandyvirgiawan4 <sandyvirgiawan4@gmail.com>
2026-06-30 08:49:09 +0700

    Update menuData.tsx

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -1,79 +1,23 @@
-import { Menu } from "@/types/menu";
+import { Menu } from "@/types/menu"; // Hapus import type ini jika file Anda menggunakan JS biasa
 
 const menuData: Menu[] = [
   {
     id: 1,
     title: "Home",
-    newTab: false,
     path: "/",
+    newTab: false,
   },
   {
     id: 2,
-    title: "Features",
-    newTab: false,
+    title: "Keunggulan",
     path: "/#features",
-  },
-  {
-    id: 2.1,
-    title: "Blog",
     newTab: false,
-    path: "/blog",
-  },
-  {
-    id: 2.3,
-    title: "Docs",
-    newTab: false,
-    path: "/docs",
   },
   {
     id: 3,
-    title: "Pages",
+    title: "Paket Internet",
+    path: "/#pricing",
     newTab: false,
-    submenu: [
-      {
-        id: 31,
-        title: "Blog Grid",
-        newTab: false,
-        path: "/blog",
-      },
-      {
-        id: 34,
-        title: "Sign In",
-        newTab: false,
-        path: "/auth/signin",
-      },
-      {
-        id: 35,
-        title: "Sign Up",
-        newTab: false,
-        path: "/auth/signup",
-      },
-      {
-        id: 35,
-        title: "Docs",
-        newTab: false,
-        path: "/docs",
-      },
-      {
-        id: 35.1,
-        title: "Support",
-        newTab: false,
-        path: "/support",
-      },
-      {
-        id: 36,
-        title: "404",
-        newTab: false,
-        path: "/error",
-      },
-    ],
-  },
-
-  {
-    id: 4,
-    title: "Support",
-    newTab: false,
-    path: "/support",
   },
 ];
 
