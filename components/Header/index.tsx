2811a437676b956bc2cbb2b2fd3abe17b00e5b87
sandyvirgiawan4 <sandyvirgiawan4@gmail.com>
2026-06-30 08:48:16 +0700

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -25,7 +25,15 @@ const Header = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", handleStickyMenu);
-  });
+    return () => window.removeEventListener("scroll", handleStickyMenu);
+  }, []);
+
+  const handleHubungiCS = () => {
+    window.open(
+      "https://wa.me/6281234567890?text=Halo%20Admin%20PT%20JTS%2C%20saya%20ingin%20bertanya%20mengenai%20layanan%20internet.",
+      "_blank"
+    );
+  };
 
   return (
     <header
@@ -40,14 +48,14 @@ const Header = () => {
           <a href="/">
             <Image
               src="/images/logo/logo-dark.svg"
-              alt="logo"
+              alt="PT JTS Logo"
               width={119.03}
               height={30}
               className="hidden w-full dark:block"
             />
             <Image
               src="/images/logo/logo-light.svg"
-              alt="logo"
+              alt="PT JTS Logo"
               width={119.03}
               height={30}
               className="w-full dark:hidden"
@@ -154,26 +162,17 @@ const Header = () => {
           <div className="mt-7 flex items-center gap-6 xl:mt-0">
             <ThemeToggler />
 
-            <Link
-              href="https://github.com/NextJSTemplates/solid-nextjs"
-              className="text-regular font-medium text-waterloo hover:text-primary"
-            >
-              GitHub Repo 🌟
-            </Link>
-
-            <Link
-              href="https://nextjstemplates.com/templates/solid"
+            <button
+              onClick={handleHubungiCS}
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
             >
-              Get Pro 🔥
-            </Link>
+              Hubungi CS
+            </button>
           </div>
         </div>
       </div>
     </header>
   );
 };
-
-// w-full delay-300
 
 export default Header;
